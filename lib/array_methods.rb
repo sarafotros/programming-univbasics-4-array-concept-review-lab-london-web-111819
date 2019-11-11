8578a536fbcd24da38acabb3029c454a43bd7bd8
Sara Fotros <sarafotros1@yahoo.com>
2019-11-11 14:10:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,4 +1,4 @@
-def find_element_index([2,3,5,7], value_to_find = 7)
+def find_element_index(array, value_to_find)
   while !value_to_find do
     break if array[counter] == value_to_find
     puts counter
