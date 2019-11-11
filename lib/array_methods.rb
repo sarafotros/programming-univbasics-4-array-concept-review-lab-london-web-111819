0841e23dca320aecc8e83689950cb1775564a319
Sara Fotros <sarafotros1@yahoo.com>
2019-11-11 14:22:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,11 +7,11 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.sort 
+  array.sort !
   puts array.last
 end
 
 def find_min_value(array)
-  array.sort
+  array.sort!
   puts array.first
 end
