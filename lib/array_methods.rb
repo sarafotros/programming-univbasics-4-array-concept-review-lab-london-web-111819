06e91200b92105aed8ab656edb063a534cfdc16c
Sara Fotros <sarafotros1@yahoo.com>
2019-11-11 14:13:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,11 +4,11 @@ def find_element_index(array, value_to_find)
     puts counter
     counter += 1 
   end  
-    
 end
 
 def find_max_value(array)
-  # Add your solution here
+  
+  
 end
 
 def find_min_value(array)
